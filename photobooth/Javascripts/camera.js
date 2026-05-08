47bffc0876cdeddea6d51d5f33074262e90f96cb
noriteriyaki <130623854+noriteriyaki@users.noreply.github.com>
2026-05-08 16:29:57 +0700

    Update camera.js

--- photobooth/Javascripts/camera.js
+++ photobooth/Javascripts/camera.js
@@ -100,7 +100,7 @@ const finalizePhotoStrip = () => {
   const { video, ctx, canvas } = elements;
   video.style.display = 'none';
   const frame = new Image();
-  frame.src = '../photobooth/Assets/tobo-photobooth/camerapage/frame.png';
+  frame.src = '../photobooth/Assets/tobo-photobooth/camerapage/frametobo2.png';
   const goNext = () => {
     try {
       localStorage.setItem('photoStrip', canvas.toDataURL('image/png'));
